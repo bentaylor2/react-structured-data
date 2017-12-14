6f47ebe0346c7493d2d61699947b8ec2e0c0f21e
Ben Taylor <ben.taylor@powerreviews.com>
2017-12-13 19:18:00 -0600

    updating dist

--- dist/schemas/ItemReviewed.js
+++ dist/schemas/ItemReviewed.js
@@ -1,26 +1,16 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-var _JSONLDAbstractNode2 = require('../JSONLDAbstractNode');
-
-var _JSONLDAbstractNode3 = _interopRequireDefault(_JSONLDAbstractNode2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+import JSONLDAbstractNode from '../JSONLDAbstractNode';
 
 var ItemReviewed = function (_JSONLDAbstractNode) {
   _inherits(ItemReviewed, _JSONLDAbstractNode);
@@ -48,6 +38,6 @@ var ItemReviewed = function (_JSONLDAbstractNode) {
   }]);
 
   return ItemReviewed;
-}(_JSONLDAbstractNode3.default);
+}(JSONLDAbstractNode);
 
-exports.default = ItemReviewed;
+export default ItemReviewed;
--- dist/schemas/index.js
+++ dist/schemas/index.js
@@ -1,124 +1,43 @@
-'use strict';
+// import AggregateRating from './AggregateRating';
+// import Author from './Author';
+// import Generic from './Generic';
+// import GenericCollection from './GenericCollection';
+// import Location from './Location';
+// import Product from './Product';
+// import Rating from './Rating';
+// import Review from './Review';
+// import Reviews from './Reviews';
+// import ItemReviewed from './ItemReviewed';
+// import Question from './Question';
+// import Answer from './Answer';
+// import Graph from './Graph';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+export { default as AggregateRating } from './AggregateRating';
+export { default as Author } from './Author';
+export { default as Generic } from './Generic';
+export { default as GenericCollection } from './GenericCollection';
+export { default as Location } from './Location';
+export { default as Product } from './Product';
+export { default as Rating } from './Rating';
+export { default as Review } from './Review';
+export { default as Reviews } from './Reviews';
+export { default as ItemReviewed } from './ItemReviewed';
+export { default as Question } from './Question';
+export { default as Answer } from './Answer';
+export { default as Graph } from './Graph';
 
-var _AggregateRating = require('./AggregateRating');
-
-Object.defineProperty(exports, 'AggregateRating', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_AggregateRating).default;
-  }
-});
-
-var _Author = require('./Author');
-
-Object.defineProperty(exports, 'Author', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Author).default;
-  }
-});
-
-var _Generic = require('./Generic');
-
-Object.defineProperty(exports, 'Generic', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Generic).default;
-  }
-});
-
-var _GenericCollection = require('./GenericCollection');
-
-Object.defineProperty(exports, 'GenericCollection', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_GenericCollection).default;
-  }
-});
-
-var _Location = require('./Location');
-
-Object.defineProperty(exports, 'Location', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Location).default;
-  }
-});
-
-var _Product = require('./Product');
-
-Object.defineProperty(exports, 'Product', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Product).default;
-  }
-});
-
-var _Rating = require('./Rating');
-
-Object.defineProperty(exports, 'Rating', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Rating).default;
-  }
-});
-
-var _Review = require('./Review');
-
-Object.defineProperty(exports, 'Review', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Review).default;
-  }
-});
-
-var _Reviews = require('./Reviews');
-
-Object.defineProperty(exports, 'Reviews', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Reviews).default;
-  }
-});
-
-var _ItemReviewed = require('./ItemReviewed');
-
-Object.defineProperty(exports, 'ItemReviewed', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_ItemReviewed).default;
-  }
-});
-
-var _Question = require('./Question');
-
-Object.defineProperty(exports, 'Question', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Question).default;
-  }
-});
-
-var _Answer = require('./Answer');
-
-Object.defineProperty(exports, 'Answer', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Answer).default;
-  }
-});
-
-var _Graph = require('./Graph');
-
-Object.defineProperty(exports, 'Graph', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Graph).default;
-  }
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// export default {
+//   AggregateRating,
+//   Author,
+//   Generic,
+//   GenericCollection,
+//   Location,
+//   Product,
+//   Rating,
+//   Review,
+//   Reviews,
+//   ItemReviewed,
+//   Question,
+//   Answer,
+//   Graph
+// }
