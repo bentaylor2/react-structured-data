9e1abac9930a68395cf1f1645e56d1e6ef91155d
Ben Taylor <ben.taylor@powerreviews.com>
2017-12-13 20:04:09 -0600

    clean up

--- dist/schemas/index.js
+++ dist/schemas/index.js
@@ -1,17 +1,3 @@
-// import AggregateRating from './AggregateRating';
-// import Author from './Author';
-// import Generic from './Generic';
-// import GenericCollection from './GenericCollection';
-// import Location from './Location';
-// import Product from './Product';
-// import Rating from './Rating';
-// import Review from './Review';
-// import Reviews from './Reviews';
-// import ItemReviewed from './ItemReviewed';
-// import Question from './Question';
-// import Answer from './Answer';
-// import Graph from './Graph';
-
 export { default as AggregateRating } from './AggregateRating';
 export { default as Author } from './Author';
 export { default as Generic } from './Generic';
@@ -25,19 +11,3 @@ export { default as ItemReviewed } from './ItemReviewed';
 export { default as Question } from './Question';
 export { default as Answer } from './Answer';
 export { default as Graph } from './Graph';
-
-// export default {
-//   AggregateRating,
-//   Author,
-//   Generic,
-//   GenericCollection,
-//   Location,
-//   Product,
-//   Rating,
-//   Review,
-//   Reviews,
-//   ItemReviewed,
-//   Question,
-//   Answer,
-//   Graph
-// }
