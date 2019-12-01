f65f3607983cdbbd19deb72cdfc1495cdb833539
gergo.bogdan@accedo.tv <gergo.bogdan@accedo.tv>
2019-12-01 18:56:54 +0100

    added Organization and PostalAddress types

--- src/core/schemas/index.js
+++ src/core/schemas/index.js
@@ -6,8 +6,12 @@ export { default as Location } from './Location';
 export { default as Product } from './Product';
 export { default as Rating } from './Rating';
 export { default as Review } from './Review';
+export { default as PostalAddress } from './PostalAddress';
+export { default as Organization } from './Organization';
 export { default as Reviews } from './Reviews';
 export { default as ItemReviewed } from './ItemReviewed';
 export { default as Question } from './Question';
 export { default as Answer } from './Answer';
 export { default as Graph } from './Graph';
+
+
